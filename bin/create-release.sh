dd869f8e521f376f0cd22694da915e890d3cee80
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2022-10-19 15:19:08 +0200

    .

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -6,6 +6,7 @@ rm -rf $TMP_DIRECTORY
 mkdir -p $TMP_RELEASE_DIRECTORY
 cp -R ../* $TMP_RELEASE_DIRECTORY
 rm -rf $TMP_RELEASE_DIRECTORY/bin/
+rm $TMP_RELEASE_DIRECTORY/languages/*.po~
 rm -rf $TMP_RELEASE_DIRECTORY/releases/
 cd $TMP_DIRECTORY
 zip -r "clp-varnish-cache-$VERSION.zip" clp-varnish-cache
