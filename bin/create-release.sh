e9435cd98d101f4845ecdde0e641df68ae3291b7
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2022-10-19 08:59:46 +0200

    .

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+VERSION='0.0.1'
 TMP_DIRECTORY='tmp'
-mkdir $TMP_DIRECTORY
+TMP_RELEASE_DIRECTORY='tmp/clp-varnish-cache/'
+rm -rf $TMP_DIRECTORY
+mkdir -p $TMP_RELEASE_DIRECTORY
+cp -R ../* $TMP_RELEASE_DIRECTORY
+rm -rf $TMP_RELEASE_DIRECTORY/bin/
+
+
+
+#rm -rf $TMP_DIRECTORY
