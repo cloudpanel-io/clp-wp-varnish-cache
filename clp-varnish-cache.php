db4422a891af83b41936bc54d472e91f667ac4e5
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2022-10-19 14:42:11 +0200

    .

--- clp-varnish-cache.php
+++ clp-varnish-cache.php
@@ -1,8 +1,8 @@
 <?php 
 /*
  * Plugin Name: CLP Varnish Cache
- * Description: Enable/Disable Varnish Cache, Purge All Pages, Cache Tags, or specific Urls
- * Version: 0.0.5
+ * Description: Varnish Cache Plugin by cloudpanel.io
+ * Version: 0.0.1
  * Text Domain: clp-varnish-cache
  * Domain Path: /languages
  * Requires at least: 6.0
@@ -16,7 +16,7 @@ if (false ===  function_exists('add_action')) {
     exit;
 }
 
-define('CLP_VARNISH_VERSION', '0.0.5');
+define('CLP_VARNISH_VERSION', '0.0.1');
 $is_admin = is_admin();
 
 if (true === $is_admin) {
