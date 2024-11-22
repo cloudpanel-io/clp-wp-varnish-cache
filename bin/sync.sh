a07ffe3c5bacaa4adbcf20d095d89c34553ea907
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2024-11-22 16:20:41 +0100

    .

--- bin/sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#rm -f /Users/$(whoami)/.ssh/known_hosts
-#rm -rf /Users/$(whoami)/Library/Application\ Support/Unison/*
-unison -prefer newer -ignore "Name .git" -ignore "Path var" -ignorearchives -repeat watch /Users/admin/PhpstormProjects/clp-wp-varnish-cache/ ssh://root@127.0.0.1//home/wordpress/htdocs/wordpress.clp/wp-content/plugins/clp-varnish-cache/ -ui text
