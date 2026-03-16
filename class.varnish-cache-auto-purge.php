7fe15aefcffd46091f7d4ff24ba8bb171eaa1759
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2026-03-16 16:36:49 +0100

    .

--- class.varnish-cache-auto-purge.php
+++ class.varnish-cache-auto-purge.php
@@ -3,16 +3,16 @@
 class ClpVarnishCacheAutoPurge {
 
     public function __construct() {
-        add_action( 'upgrader_process_complete', array( $this, 'auto_purge' ), 10, 2 );
-        add_action( 'deactivated_plugin', array( $this, 'auto_purge' ) );
-        add_action( 'activated_plugin', array( $this, 'auto_purge' ) );
-        add_action( 'switch_theme', array( $this, 'auto_purge' ) );
+        add_action('upgrader_process_complete', array($this, 'auto_purge' ), 10, 2);
+        add_action('deactivated_plugin', array($this, 'auto_purge'));
+        add_action('activated_plugin', array($this, 'auto_purge'));
+        add_action('switch_theme', array($this, 'auto_purge'));
     }
 
     public function auto_purge( $upgrader_or_plugin = null, $options = null ) {
         if (current_action() === 'upgrader_process_complete') {
             if (empty($options['action']) || $options['action'] !== 'update' ) return;
-            if (empty($options['type']) || ! in_array( $options['type'], ['plugin', 'theme', 'core'], true ) ) return;
+            if (empty($options['type']) || ! in_array( $options['type'], ['plugin', 'theme', 'core'], true)) return;
         }
 
         if (!class_exists( 'ClpVarnishCacheManager')) return;
@@ -20,8 +20,8 @@ class ClpVarnishCacheAutoPurge {
 
         if (!$manager->is_enabled()) return;
 
-        $host = wp_parse_url( home_url(), PHP_URL_HOST );
-        if (!empty($host) ) $manager->purge_host($host);
+        $host = wp_parse_url(home_url(), PHP_URL_HOST);
+        if (!empty($host)) $manager->purge_host($host);
 
         $prefix = $manager->get_cache_tag_prefix();
         if (!empty($prefix)) $manager->purge_tag($prefix);
